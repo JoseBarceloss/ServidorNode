6ae9ce0fdd679c7d98a6effcfeac2d1ed09164b8
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-03-05 05:39:05 -0300

    Colocando alguma coisa na porta 3301 :D

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.status(200).json(
+    { message: 'Aqui vai ficar sua API barcelos :) em HyperText Tranfrer Protocol (HTTP)' },
+));
+
 module.exports = app;
